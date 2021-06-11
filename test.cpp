a0dc71912d469e4bdeb7e08506306d396ccf18e7
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2021-06-11 02:16:48 -0400

    switch to cone and add comments on why it didn't work before

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ VTK_MODULE_INIT(vtkRenderingFreeType);
 VTK_MODULE_INIT(vtkInteractionStyle);
 
 #include <vtkSmartPointer.h>
-#include <vtkCylinderSource.h>
+#include <vtkConeSource.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkTextActor.h>
 #include <vtkRenderer.h>
@@ -15,21 +15,30 @@ VTK_MODULE_INIT(vtkInteractionStyle);
 
 int main()
 {
-    auto cyl = vtkSmartPointer<vtkCylinderSource>::New();
+    auto cone = vtkSmartPointer<vtkConeSource>::New();
+    cone->SetResolution(10);
+
 
     auto mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->SetInputConnection(cyl->GetOutputPort());
-    // SetInputData? GetOutput?
+    mapper->SetInputConnection(cone->GetOutputPort());
+    // Previously I was doing this and it wasn't working:
+    // mapper->SetInputData(cone->GetOutput());
+    // Here the object being passed is a vtKPolyData*,
+    // whereas in the above thing that works the object is vtkAlgorithmOutput*
+    // Some explanation here about the recommended way to do it now:
+    // https://vtk.org/Wiki/VTK/Tutorials/PipelineInput
+    // Some explanation here about why one way works and the other doesn't, but in Chinese:
+    // https://blog.csdn.net/yuyangyg/article/details/78165570
 
-    auto cylActor = vtkSmartPointer<vtkActor>::New();
-    cylActor->SetMapper(mapper);
+    auto coneActor = vtkSmartPointer<vtkActor>::New();
+    coneActor->SetMapper(mapper);
 
     auto textActor = vtkSmartPointer<vtkTextActor>::New();
     textActor->SetInput("Hello World");
 
     auto renderer = vtkSmartPointer<vtkRenderer>::New();
     renderer->AddActor(textActor);
-    renderer->AddActor(cylActor);
+    renderer->AddActor(coneActor);
 
     auto interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
     
